a11e7462cefcc75bec946cf904f8ee6e7f6a6131
Shlok0505 <shlokskamble@gmail.com>
2025-05-13 01:27:30 +0530

    Add files via upload

---
+++ message.js
@@ -0,0 +1,35 @@
+// // function msg(){
+// //     alert("Welcome to javascript workshop!!!!!!");
+// // }
+// // let age = prompt("Enter your age");
+// // if(age >= 18){
+// //     msg(shlok);
+// // }else{
+// //     alert("You are not eligible");
+// // } 
+// // let pass = 12345678;
+// // function auth(uid, password) {
+// //     if (uid !== "Whiteshlok") {
+// //         console.log("Incorrect id");
+// //     } else if (password !== 12345678) {
+// //         console.log("Incorrect password");
+// //     } else {
+// //         console.log("Access granted");
+// //     }
+// // }
+
+
+// // auth("Whiteshlok", 12345678);
+
+
+// let value = 7;
+// for (let i=0; i<=10 ; i++){
+//     console.log(`${value}*${i}=${value * i}`)
+// }
+let value = 7;
+let i = 0;
+
+while (i <= 10) {
+    console.log(`${value}*${i}=${value * i}`);
+    i++;
+}
